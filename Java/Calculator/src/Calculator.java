8a2e78b1cdc6d33e5c7b3b901486543ae0ba61fb
Aleandro Blanco <lablanco03@gmail.com>
2026-09-01 11:09:00 -0600

    feat: add custom colors, label and display panel

--- Java/Calculator/src/Calculator.java
+++ Java/Calculator/src/Calculator.java
@@ -9,15 +9,34 @@ public class Calculator {
     int boardWidth = 360;
     int boardHeight = 540;
 
+    Color customLightGray = new Color(212, 212, 210);
+    Color customDarkGray = new Color(80, 80, 80);
+    Color customBlack = new Color(28, 28, 28);
+    Color customOrange = new Color(255, 149, 0);
+
     JFrame frame = new JFrame("Calculator");
+    JLabel displayLabel = new JLabel();
+    JPanel displayPanel = new JPanel();
 
     Calculator() {
+
         frame.setVisible(true);
         frame.setSize(boardWidth, boardHeight);
         frame.setLocationRelativeTo(null);
         frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(new BorderLayout());
+    
+        displayLabel.setBackground(customBlack);
+        displayLabel.setForeground(Color.white);
+        displayLabel.setFont(new Font("Arial", Font.PLAIN, 80));
+        displayLabel.setHorizontalAlignment(JLabel.RIGHT);
+        displayLabel.setText("0");
+        displayLabel.setOpaque(true);
+
+        displayPanel.setLayout(new BorderLayout());
+        displayPanel.add(displayLabel);
+        frame.add(displayPanel);
     }
 
 

a4891f8fa5aee44910dc3aefc74086bc6e81db83
Aleandro Blanco <lablanco03@gmail.com>
2026-09-03 08:41:50 -0600

    feat: implementation of the % button

--- Java/Calculator/src/Calculator.java
+++ Java/Calculator/src/Calculator.java
@@ -114,7 +114,9 @@ public class Calculator {
                             displayLabel.setText(removeZeroDecimal(numDisplay));
                         }
                         else if (buttonValue.equals("%")) {
-
+                            double numDisplay = Double.parseDouble(displayLabel.getText());
+                            numDisplay /= 100;
+                            displayLabel.setText(removeZeroDecimal(numDisplay)); 
                         }
 
                     }
